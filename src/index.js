a524a11a3c47744ea72b8578df82a94186464455
anurag-crownstack <anurag.singh@crownstack.com>
2024-10-19 21:18:17 +0530

    added vercel file

--- src/index.js
+++ src/index.js
@@ -4,22 +4,11 @@ const cors = require("cors");
 require('dotenv').config();
 const videoRoutes = require('../src/routes/videoRoute');
 
-// app.use(cors());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 // video routes
 app.use('/api', videoRoutes);
-
-const corsOptions = {
-    origin: '*', // or specify frontend URL here, e.g., 'https://example.com'
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-};
-
-// Enable CORS with options
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // Allow preflight for all routes
-
 app.get('/', (req, res) => {
     res.send('Welcome to the backend!');
 });
