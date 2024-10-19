9980283c1da2483a4ff58de1130bd84e1a5524b5
anurag-crownstack <anurag.singh@crownstack.com>
2024-10-19 22:54:22 +0530

    change in gitignore

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ const cors = require("cors");
 require('dotenv').config();
 const videoRoutes = require('../src/routes/videoRoute');
 
-app.use(cors());
+// app.use(cors());
+const corsOptions = {
+    origin: 'https://media-player-frontend-beta.vercel.app', // Replace with your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, // If you're dealing with cookies/sessions
+    allowedHeaders: ['Content-Type', 'Authorization'] // Customize as needed
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 
 // video routes
@@ -12,6 +21,7 @@ app.use('/api', videoRoutes);
 app.get('/', (req, res) => {
     res.send('Welcome to the backend!');
 });
+
 const port = process.env.PORT || 8000;
 
 app.listen(port, function () {
